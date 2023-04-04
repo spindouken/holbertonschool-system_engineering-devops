57ce0365ee9eb00ce838f33208a2f731b4bc8003
spindouken <mlc9181209@gmail.com>
2023-04-03 22:06:08 -0500

    refactor

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python3
 """This script gathers data from the JSONPlaceholder API to track the progress
 of an employee's tasks"""
+
+import json
 import requests
 import sys
-import urllib
-
-
-def get_employee_todo_progress(employee_id):
-    """docstring placeholder"""
-    # Make a GET request to retrieve the employee data
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_response = requests.get("{}/users/{}"
-                                 .format(base_url, employee_id))
-    user_data = user_response.json()
-
-    if 'name' not in user_data:
-        print("Invalid employee ID")
-        return
-
-    todos_response = requests.get("{}/users/{}/todos"
-                                  .format(base_url, employee_id))
-    todos_data = todos_response.json()
-
-    done_tasks = [task for task in todos_data if task["completed"]]
-    total_tasks = len(todos_data)
-
-    print("Employee {} is done with tasks({}/{}): "
-          .format(user_data['name'], len(done_tasks), total_tasks))
-
-    for task in done_tasks:
-        print("\t", task["title"])
-
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    """Main function to gather data from the
+    JSONPlaceholder API for an employee's tasks.
+    ...retrieves data from the API for a specific employee based on the
+    user ID provided as a command-line argument.
+    ...then extracts the employee's name and the number
+    of tasks the employee has completed out of the total number of tasks.
+    Finally, it prints the employee's name along with
+    the number of completed tasks and their titles.
+    """
+    num_done, num_tasks = 0, 0
+    userId = sys.argv[1]
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
+    # create Response object for specific user and that user's tasks
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(userId)
+    user_response = requests.get(url)
 
-    get_employee_todo_progress(employee_id)
+    url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
+        .format(userId)
+    todo_response = requests.get(url)
+
+    # create Dictionary objects from response objects
+    user_info = json.loads(user_response.text)
+    todo_info = json.loads(todo_response.text)
+
+    employee_name = user_info['name']
+
+    for task in todo_info:
+        num_tasks += 1
+        if task['completed']:
+            num_done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_done, num_tasks))
+
+    for task in todo_info:
+        if task['completed']:
+            print("\t {}".format(task['title']))
