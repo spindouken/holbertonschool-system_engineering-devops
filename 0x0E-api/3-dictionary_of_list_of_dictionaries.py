48689c478eb870fb7c37671447dcc00cf6450cae
spindouken <mlc9181209@gmail.com>
2023-04-03 22:26:23 -0500

    refactored version

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,62 @@
 #!/usr/bin/python3
+"""
+exports information about an employees TODO list progress
+given an employee id to a CSV file named `USER_ID.csv`.
+"""
 import json
 import requests
 
 
-def get_all_employees_tasks():
-    # Get all employee data
-    all_employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
-    all_employee_data = all_employee_data.json()
+if __name__ == "__main__":
+    """Main function to retrieve and
+    export all employees' tasks from
+    JSONPlaceholder API.
 
-    # Prepare JSON data
-    json_data = {}
-    for employee_data in all_employee_data:
-        employee_id = employee_data['id']
-        
-        # Get employee TODOs
-        employee_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-        employee_todos = employee_todos.json()
+    This function retrieves data from
+    the JSONPlaceholder API for all users' tasks.
+    For each user, creates a dictionary where the
+    keys correspond to the user ID and the values
+    correspond to a list of dictionaries,
+    where each dictionary corresponds to
+    a task and contains information such as
+    the task title, completion status,
+    and username. Finally, it writes
+    the dictionary to a JSON file
+    named 'todo_all_employees.json'.
+    """
 
-        tasks = []
-        for task in employee_todos:
-            tasks.append(
-                {
-                    "username": employee_data["username"],
-                    "task": task["title"],
-                    "completed": task["completed"]
-                }
-            )
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    user_response = requests.get(url)
+    user_info = json.loads(user_response.text)
 
-        json_data[str(employee_id)] = tasks
+    # create dictionary containing users and their associated tasks
+    tasks = {}
 
-    return json_data
+    for user in user_info:
+        user_id = user['id']
 
-def export_all_employee_tasks_to_json(json_data):
-    # Export JSON data
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(json_data, json_file)
+        # create Response object for specific user and that user's tasks
+        url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
+            .format(user_id)
+        todo_response = requests.get(url)
 
-if __name__ == '__main__':
-    json_data = get_all_employees_tasks()
-    export_all_employee_tasks_to_json(json_data)
+        # create Dictionary objects from response objects
+        todo_info = json.loads(todo_response.text)
+
+        tasks_list = []
+
+        # create a dictionary for each task and add it to the list of tasks
+        for task in todo_info:
+            task_dict = {}
+            task_dict['username'] = user['username']
+            task_dict['task'] = task['title']
+            task_dict['completed'] = task['completed']
+            tasks_list.append(task_dict)
+
+        # add list of tasks to dictionary for that specific user
+        tasks[user_id] = tasks_list
+
+    # write dictionary to a JSON file named 'todo_all_employees.json'
+    with open('todo_all_employees.json', 'w', encoding='UTF8',
+              newline='') as f:
+        f.write(json.dumps(tasks))
